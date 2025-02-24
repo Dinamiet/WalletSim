206d839f26cae3335c94111fc42cc3a9fcaa079a
Dinamiet <iceman.sm39@gmail.com>
2025-02-24 20:17:55 +0200

    Formatting

--- src/wallet_sim.c
+++ src/wallet_sim.c
@@ -1,62 +1,53 @@
 #include "wallet_sim.h"
 
-void       Wallet_Init(Wallet* wallet, Order* pendingOrders, size_t listLength)
+void Wallet_Init(Wallet* wallet, Order* pendingOrders, size_t listLength)
 {
-	wallet->LocalFree = 0.0f;
+	wallet->LocalFree   = 0.0f;
 	wallet->ForeignFree = 0.0f;
-	wallet->Fees = 0.0f;
+	wallet->Fees        = 0.0f;
 
 	BufferedList_Init(&wallet->Pending, pendingOrders, sizeof(*pendingOrders), listLength);
-
 }
 
-void       Wallet_SetFees(Wallet* wallet, float fees)
-{
-	wallet->Fees = fees;
-}
+void Wallet_SetFees(Wallet* wallet, float fees) { wallet->Fees = fees; }
 
-void       Wallet_AddLocal(Wallet* wallet, float amount)
-{
-	wallet->LocalFree += amount;
-}
+void Wallet_AddLocal(Wallet* wallet, float amount) { wallet->LocalFree += amount; }
 
-void       Wallet_AddForeign(Wallet* wallet, float amount)
-{
-	wallet->ForeignFree += amount;
-}
+void Wallet_AddForeign(Wallet* wallet, float amount) { wallet->ForeignFree += amount; }
 
-bool       Wallet_PlaceOrder(Wallet* wallet, OrderType type, float amount, float price)
+bool Wallet_PlaceOrder(Wallet* wallet, OrderType type, float amount, float price)
 {
 	switch (type)
 	{
-	case WALLET_SELL:
-		if (wallet->ForeignFree > amount)
-		{
-			Order* newOrder = BufferedList_AddEnd(&wallet->Pending);
-			if (!newOrder)
-				return false;
+		case WALLET_SELL:
+			if (wallet->ForeignFree > amount)
+			{
+				Order* newOrder = BufferedList_AddEnd(&wallet->Pending);
+				if (!newOrder)
+					return false;
 
-			newOrder->Amount = amount;
-			newOrder->Type = type;
-			newOrder->Price = price;
+				newOrder->Amount = amount;
+				newOrder->Type   = type;
+				newOrder->Price  = price;
 
-			return true;
-		}
-		break;
+				return true;
+			}
+			break;
 
-	case WALLET_BUY:
-		if (wallet->LocalFree > (amount * price))
-		{
-			Order* newOrder = BufferedList_AddEnd(&wallet->Pending);
-			if (!newOrder)
-				return false;
+		case WALLET_BUY:
+			if (wallet->LocalFree > (amount * price))
+			{
+				Order* newOrder = BufferedList_AddEnd(&wallet->Pending);
+				if (!newOrder)
+					return false;
 
-			newOrder->Amount = amount;
-			newOrder->Type = type;
-			newOrder->Price = price;
+				newOrder->Amount = amount;
+				newOrder->Type   = type;
+				newOrder->Price  = price;
 
-			return true;
-		}
+				return true;
+			}
+			break;
 	}
 
 	return false;
@@ -97,10 +88,7 @@ void Wallet_ExecuteOrders(Wallet* wallet, float currentPrice)
 	}
 }
 
-Order* Wallet_GetPendingOrders(Wallet* wallet)
-{
-	return BufferedList_First(&wallet->Pending);
-}
+Order* Wallet_GetPendingOrders(Wallet* wallet) { return BufferedList_First(&wallet->Pending); }
 
 Balance Wallet_GetBalance(Wallet* wallet)
 {
